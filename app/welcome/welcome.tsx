40fa5185702ffed8a8154119dbefc704ceb71450
Victor López <victorlopez_20@outlook.com>
2025-12-09 15:56:40 -0600

    New: Creating layouts & components for the landing tructure

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,54 +1,31 @@
-import headerImage from '../images/header-image.png';
+import Navbar from '../design/Navbar';
+import Header from '../components/main/header';
+import ServicesSection from '../content/home/ServicesSection';
+import ContactSection from '../content/home/ContactSection';
 
 export function Welcome() {
 	return (
 		<>
 			{/** Navbar (Menú de navegación) */}
-			<div className="navbar">
-				<div className="navbar-container">
-					<span className="navbar-item">
-						<a className="navbar-link" href="/">
-							Inicio
-						</a>
-					</span>
-					<span className="navbar-item">
-						<a className="navbar-link" href="/">
-							Servicios
-						</a>
-					</span>
-					<span className="navbar-item">
-						<a className="navbar-link" href="/">
-							Contacto
-						</a>
-					</span>
-					<span className="navbar-item">
-						<a className="navbar-link" href="/">
-							Nosotros
-						</a>
-					</span>
-				</div>
-			</div>
+			<Navbar></Navbar>
 
 			{/* Header (Encabezado) */}
-			<header className="header">
-				<div className="header-container">
-					<div className="w-2/5">
-						<img
-							src={headerImage}
-							className="w-2/3 object-center"
-							alt=""
-						/>
-					</div>
-					<div className="w-3/5">
-						<h1 className="header-title text-5xl font-bold mb-5">
-							Tu mejor elección a la hora de comprar
-						</h1>
-						<p className="text-base text-justify">
-							lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-						</p>
-					</div>
-				</div>
-			</header>
+			<Header></Header>
+
+			{/* Main (Contenido principal) */}
+			<main>
+
+				{/* Sección de Servicios */}
+				<ServicesSection></ServicesSection>
+
+				{/* Sección de contacto */}
+				<ContactSection></ContactSection>
+			</main>
+
+			{/* Footer (Pie de página) */}
+			<footer>
+				Pie de página
+			</footer>
 		</>
 	);
 }
